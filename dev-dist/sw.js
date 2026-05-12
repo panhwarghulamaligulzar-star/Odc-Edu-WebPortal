a988d6f1739d19b1190bee99c57de5363d117914
panhwarghulamaligulzar-star <panhwar.ghulamali.gulzar@gmail.com>
2026-05-12 21:13:56 +0500

    Use port 5028, import reg no util, bump SW rev
    Switch development API/backend URLs to port 5028 in .env and .env.development to align local dev servers. Add import for generateRegistrationNo in enrollmentController (prepares usage of the utility). Update service worker precache revision in dev-dist/sw.js.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5ikr29onnb8"
+    "revision": "0.980a6upvv5g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
