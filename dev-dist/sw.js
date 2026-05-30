63fed042aa7322b15de73ec6e2d09a2d6be3e500
panhwarghulamaligulzar-star <panhwar.ghulamali.gulzar@gmail.com>
2026-05-31 03:38:15 +0500

    super admin setup

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.f4hk1svepuo"
+    "revision": "0.9nd17ndq2so"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
