536e637038aab27223a21cdd39b64f0aecd154d8
panhwarghulamaligulzar-star <panhwar.ghulamali.gulzar@gmail.com>
2026-06-16 22:40:50 +0500

    deploy agin

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.q1ess8h84vg"
+    "revision": "0.49b070hhj9"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
