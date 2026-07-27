8460d7359664eb5744879a1b2fba62a8135fd045
panhwarghulamaligulzar-star <panhwar.ghulamali.gulzar@gmail.com>
2026-07-27 19:57:14 +0500

    new bulid

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2tgt19st8co"
+    "revision": "0.fhqmdh8k91o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
