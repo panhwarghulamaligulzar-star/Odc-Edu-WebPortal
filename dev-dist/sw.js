7c7923ffce082ab5d7c5222cf05a6d46b9b547e0
panhwarghulamaligulzar-star <panhwar.ghulamali.gulzar@gmail.com>
2026-08-27 23:22:44 +0500

    new build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.m0n5d8aek68"
+    "revision": "0.dsb6dh09dfg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
